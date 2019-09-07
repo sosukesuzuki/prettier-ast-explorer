0011f00a3c7a1872d88871bd682bdeff9661da38
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-07 22:17:18 +0900

    Fix style

--- src/components/templates/TopNav.tsx
+++ src/components/templates/TopNav.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.header`
-    border-bottom: 1px solid rgb(225, 228, 232);
     padding: 10px;
     font-size: 10px;
+    background-color: #3d3d3d;
     h1 {
         margin: 0;
+        color: white;
     }
 `;
 

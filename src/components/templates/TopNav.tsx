6cebba8e11241e377cd9f96df20291c683302062
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-07 22:43:36 +0900

    Implment dropDown for parser

--- src/components/templates/TopNav.tsx
+++ src/components/templates/TopNav.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { Dropdown } from '@primer/components';
+import { parsers } from '../../lib/constants';
+import { parserContext } from '../../lib/contexts';
 
 const Container = styled.header`
     padding: 10px;
     font-size: 10px;
     background-color: #3d3d3d;
+    display: flex;
+    justify-content: space-between;
     h1 {
         margin: 0;
         color: white;
@@ -12,9 +17,25 @@ const Container = styled.header`
 `;
 
 const TopNav: React.FC = () => {
+    const { parser, setParser } = useContext(parserContext);
     return (
         <Container>
             <h1>AST Exploler</h1>
+            <Dropdown title={`parser: ${parser}`}>
+                <Dropdown.Menu direction="sw" title={`parser: ${parser}`}>
+                    {parsers.map(parser => (
+                        <Dropdown.Item key={parser}>
+                            <div
+                                onClick={() => {
+                                    setParser(parser);
+                                }}
+                            >
+                                {parser}
+                            </div>
+                        </Dropdown.Item>
+                    ))}
+                </Dropdown.Menu>
+            </Dropdown>
         </Container>
     );
 };
--- src/lib/constants/parser.ts
+++ src/lib/constants/parser.ts
@@ -1,2 +1,4 @@
 export const BABEL = 'babel';
 export const TS = 'typescript';
+
+export const parsers: [typeof BABEL, typeof TS] = [BABEL, TS];

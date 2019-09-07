36c573bffb1f68a8ac4c7037585f496739bf5719
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-08 04:27:07 +0900

    Use webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+const MODE = process.env.NODE_ENV || 'development';
+const DEV = MODE === 'development';
+
+const copyRules = [
+    {
+        from: __dirname + '/src/index.html',
+        to: __dirname + '/dist/index.html',
+    },
+];
+
+module.exports = {
+    mode: MODE,
+    devtool: DEV ? 'inline-source-map' : 'source-map',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'style-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [new CopyPlugin(copyRules)],
+};

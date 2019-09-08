c104c0bfa6f937f27d95874fbb98c1818703ef8f
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-08 14:36:17 +0900

    Improve showing syntax wrror

--- src/components/templates/ASTPreviewer.tsx
+++ src/components/templates/ASTPreviewer.tsx
@@ -8,12 +8,18 @@ import {
 } from '../../lib/contexts';
 import { theme } from '../../lib/constants';
 import { Flash } from '@primer/components';
+import { usePrevious } from '../../lib/hooks';
 
 const Container = styled.div`
     width: 50%;
     white-space: pre-wrap;
     overflow-y: scroll;
     background-color: #1c2023;
+`;
+const FixedFlash = styled(Flash)`
+    position: fixed;
+    bottom: 30px;
+    z-index: 1;
 `;
 
 const ASTPreviewer: React.FC = () => {
@@ -22,6 +28,7 @@ const ASTPreviewer: React.FC = () => {
     const { parser } = useContext(parserContext);
     const { source } = useContext(sourceContext);
     const proxy = useContext(workerContext);
+    const prevAst = usePrevious(ast);
 
     useEffect(() => {
         setError('');
@@ -37,15 +44,18 @@ const ASTPreviewer: React.FC = () => {
 
     return (
         <Container>
-            {error || !ast ? (
-                <Flash scheme="red">{error || '...Waiting Worker'}</Flash>
-            ) : (
+            {(ast || prevAst) && (
                 <JSONTree
-                    data={ast}
+                    data={ast || prevAst}
                     theme={theme}
                     invertTheme={false}
                     shouldExpandNode={() => true}
                 />
+            )}
+            {(error || (!ast && !prevAst)) && (
+                <FixedFlash scheme="red">
+                    {error || '...Waiting Worker'}
+                </FixedFlash>
             )}
         </Container>
     );

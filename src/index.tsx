8888d3c45aa90337125e677ade5b8924bddd4f09
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-08 14:15:05 +0900

    Remove unnecesarry ,

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ async function main() {
 
     ReactDOM.render(
         <workerContext.Provider value={proxy}>
-            <App />,
+            <App />
         </workerContext.Provider>,
         document.querySelector('.root'),
     );
